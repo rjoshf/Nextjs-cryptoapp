13da787e21ffe94448eade57d8eabdbfe3ee664c
rjoshf <joshwalters049@gmail.com>
2024-02-19 21:28:19 +0000

    Updated styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
-import { BiBitcoin } from "react-icons/bi";
-import { FaEthereum } from "react-icons/fa";
+import Image from 'next/image';
+
+import bitcoinImg from '../../public/bitcoin.svg'
+import ethereumImg from '../../public/ethereum.svg'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="flex flex-row items-center">
-        <BiBitcoin className="bitcoinicon" />
+        <Image className="mr-10 bounce" width={75} height={75} src={bitcoinImg} alt={""}></Image>
         <h1 className="font-bold">Next Crypto</h1>
-        <FaEthereum className="ethereumicon" />
+        <Image className="ml-10 bounce" width={75} height={75} src={ethereumImg} alt={""}></Image>
       </div>
     </main>
   );
