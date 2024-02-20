ef53d1deb0ef72dfdf1b3549230072223778e4e8
rjoshf <joshwalters049@gmail.com>
2024-02-20 23:31:03 +0000

    Added images for the crypto table

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import MarketFetcher from "@/components/MarketFetcher";
 import { Suspense } from "react";
 
 export default function Home() {
-  console.log(process.env.API_KEY)
+
   return (
     <main className="min-h-screen">
       <Header />
--- src/components/MarketTable.tsx
+++ src/components/MarketTable.tsx
@@ -1,13 +1,17 @@
+import Image from 'next/image';
 
 interface Crypto {
     id: string;
+    image: string;
     name: string;
     current_price: number;
     price_change_percentage_24h: number;
     market_cap: number;
 }
 
+
 const MarketTable: React.FC<{ cryptos: Crypto[] }> = ({ cryptos }) => {
+
     return (
         <div className="market-table-container">
             <div className="market-table-header">
@@ -18,10 +22,13 @@ const MarketTable: React.FC<{ cryptos: Crypto[] }> = ({ cryptos }) => {
             </div>
             <ul className="market-table">
                 {cryptos.map(crypto => <li className="text-black market-table-item" key={crypto.id}>
-                    <div>{crypto.name}</div>
-                    <div>{crypto.current_price}</div>
-                    <div>{crypto.price_change_percentage_24h}</div>
-                    <div>{crypto.market_cap}</div>
+                    <div className="coinName">
+                        <Image className="mr-5" width={30} height={30} src={crypto.image} alt={crypto.name} />
+                        <div>{crypto.name}</div>
+                    </div>
+                    <div>{`${new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD', maximumFractionDigits: 2 }).format(crypto.current_price)}`}</div>
+                    <div style={{ color: crypto.price_change_percentage_24h > 0 ? 'lightgreen' : 'red' }}>{crypto.price_change_percentage_24h}</div>
+                    <div>{`${new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD', maximumFractionDigits: 2 }).format(crypto.market_cap)}`}</div>
                 </li>)}
             </ul>
         </div>
