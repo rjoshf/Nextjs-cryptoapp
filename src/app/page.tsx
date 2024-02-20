2946e4ee9ec59952f8b7bf8a7cbc390d16faa29f
rjoshf <joshwalters049@gmail.com>
2024-02-20 00:28:50 +0000

    Added sign up section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,14 @@ import ethereumImg from '../../public/ethereum.svg'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center p-24">
       <div className="flex flex-row items-center">
         <Image className="mr-10 bounce" width={75} height={75} src={bitcoinImg} alt={""}></Image>
-        <h1 className="font-bold">Next Crypto</h1>
+        <h1 className="font-bold">NextCrypto</h1>
         <Image className="ml-10 bounce" width={75} height={75} src={ethereumImg} alt={""}></Image>
       </div>
+      <button className="signupButton mt-14">Sign Up</button>
+      <h2 className="font-bold text-5xl text-white mt-14"><span className="text-purple-800">Sign up</span> to create your <span className="text-fuchsia-700">wallet!</span></h2>
     </main>
   );
 }
