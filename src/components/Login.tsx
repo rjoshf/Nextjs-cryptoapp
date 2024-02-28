2b7f2af2d384c7a44071f3ce18630b27017612a1
rjoshf <joshwalters049@gmail.com>
2024-02-28 18:24:22 +0000

    Updated styling

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -12,26 +12,28 @@ const Login: React.FC<{}> = ({ }) => {
     return (
         <>
             <h1 className="text-center">Login</h1>
-            <form className="flex justify-center items-center flex-col">
-                <div>
-                    <label className="block text-center" htmlFor='email'>Your Email: </label>
-                    <input type='email' id='email' required />
-                </div>
-                <div>
-                    <label className="block text-center" htmlFor='password'>Your Password: </label>
-                    <input type='password' id='password' required />
-                </div>
-                <div className="flex flex-col items-center justify-center">
-                    <button>{isLogin ? 'Login' : 'Create Account'}</button>
-                    <button
-                        type='button'
-                        onClick={switchAuthModeHandler}
-                        className="block"
-                    >
-                        {isLogin ? 'Create new account' : 'Login with existing account'}
-                    </button>
-                </div>
-            </form>
+            <section className="info-card w-4/12 m-auto bg-purple-800 bg-opacity-15 rounded-lg py-5 px-5">
+                <form className="flex justify-center items-center flex-col">
+                    <div>
+                        <label className="block text-center mb-5 text-fuchsia-700 font-bold" htmlFor='email'>Your Email: </label>
+                        <input className="text-black" type='email' id='email' required />
+                    </div>
+                    <div>
+                        <label className="block text-center my-5 text-fuchsia-700 font-bold" htmlFor='password'>Your Password: </label>
+                        <input className="text-black" type='password' id='password' required />
+                    </div>
+                    <div className="flex flex-col items-center justify-center">
+                        <button className="m-5 py-3 px-8 rounded-lg bg-fuchsia-700 loginButton font-bold">{isLogin ? 'Login' : 'Create Account'}</button>
+                        <button
+                            type='button'
+                            onClick={switchAuthModeHandler}
+                            className="block text-fuchsia-700"
+                        >
+                            {isLogin ? 'Create new account' : 'Login with existing account'}
+                        </button>
+                    </div>
+                </form>
+            </section>
         </>
     )
 }
