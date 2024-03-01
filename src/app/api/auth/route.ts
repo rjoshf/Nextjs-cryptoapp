225ad7d53bef73611af8902306fcff66cea657d3
rjoshf <joshwalters049@gmail.com>
2024-03-01 17:29:58 +0000

    Added post request logic for creating a new user

--- src/app/api/auth/route.ts
+++ src/app/api/auth/route.ts
@@ -2,37 +2,40 @@ import { NextRequest, NextResponse } from "next/server";
 import { connectToDatabase } from "../../../../lib/db";
 import { hashPassword } from "../../../../lib/auth";
 
+export async function POST(req: NextRequest, res: NextResponse) {
+    try {
+        const data = await req.json();
 
-async function handler(req: NextRequest, res: NextResponse) {
+        const { email, password } = data;
 
-    const data = req.body;
+        if (!email || !email.includes('@') || !password || password.trim().length < 7) {
+            return NextResponse.json({
+                message: 'Invalid input - password should also be at least 7 characters long.',
+            }, {
+                status: 500,
+            });
+        }
 
-    const {email, password} = data;
+        const client = await connectToDatabase();
+        const db = client.db();
+        const hashedPassword = await hashPassword(password);
 
-    if (!email || !email.includes('@') || !password || password.trim().length < 7) {
+        const result = await db.collection('users').insertOne({
+            email: email,
+            password: hashedPassword,
+        });
+
         return NextResponse.json({
-            message: 'Invalid input - password should also be at least 7 characters long.',
+            message: 'Created user!',
+        }, {
+            status: 201,
+        });
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({
+            message: 'Internal server error.',
         }, {
             status: 500,
         });
     }
-
-    const client = await connectToDatabase();
-
-    const db = client.db();
-
-    const hashedPassword = hashPassword(password)
-
-    const result = await db.collection('users').insertOne({
-        email: email,
-        password: hashedPassword,
-    });
-
-    return NextResponse.json({
-        message: 'Created user!',
-    }, {
-        status: 201,
-    });
 }
-
-export default handler;
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,28 +1,70 @@
 'use client'
 
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 import { motion } from 'framer-motion';
 
+async function createUser(email: string, password: string) {
+    const response = await fetch('/api/auth', {
+        method: 'POST',
+        body: JSON.stringify({ email, password }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    const data = response.json();
+
+    if (!response.ok) {
+        throw new Error('Something went wrong.');
+    }
+
+    return data;
+}
+
 const Login: React.FC<{}> = ({ }) => {
+    const emailInputRef = useRef<HTMLInputElement>(null);
+    const passwordInputRef = useRef<HTMLInputElement>(null);
     const [isLogin, setIsLogin] = useState(true);
 
     function switchAuthModeHandler() {
         setIsLogin((prevState) => !prevState);
     }
 
+    async function submitHandler(event: React.FormEvent) {
+        event.preventDefault();
+
+        const enteredEmail = emailInputRef.current?.value;
+        const enteredPassword = passwordInputRef.current?.value;
+
+        if (enteredEmail === undefined || enteredPassword === undefined) {
+            return;
+        }
+
+        if (isLogin) {
+            // log user in
+        } else {
+            try {
+                const result = await createUser(enteredEmail, enteredPassword);
+                console.log(result);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+    }
+
     return (
         <>
             <motion.section viewport={{ once: true, amount: 0.8 }} initial={{ opacity: 0, y: 15, scale: 0.99 }} whileInView={{ opacity: 1, y: 0, scale: 1 }} transition={{ type: 'tween', duration: 0.75 }}>
                 <h1 className="text-center mb-8 font-bold">Login</h1>
-                <form className="flex justify-center items-center flex-col info-card w-4/12 m-auto bg-purple-800 bg-opacity-15 rounded-lg py-5 px-5">
+                <form onSubmit={submitHandler} className="flex justify-center items-center flex-col info-card w-4/12 m-auto bg-purple-800 bg-opacity-15 rounded-lg py-5 px-5">
                     <div>
                         <label className="block text-center mb-5 text-fuchsia-700 font-bold" htmlFor='email'>Your Email: </label>
-                        <input className="text-black" type='email' id='email' required />
+                        <input className="text-black" type='email' id='email' ref={emailInputRef} required />
                     </div>
                     <div>
                         <label className="block text-center my-5 text-fuchsia-700 font-bold" htmlFor='password'>Your Password: </label>
-                        <input className="text-black" type='password' id='password' required />
+                        <input className="text-black" type='password' id='password' required ref={passwordInputRef} />
                     </div>
                     <div className="flex flex-col items-center justify-center">
                         <motion.button whileHover={{ scale: 1.03 }} transition={{ type: 'spring', stiffness: 100 }} className="m-5 py-3 px-8 rounded-lg bg-fuchsia-700 loginButton font-bold">{isLogin ? 'Login' : 'Create Account'}</motion.button>
