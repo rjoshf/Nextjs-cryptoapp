7b5d25d67240e6e673b063faa93b6cfece87c3e7
rjoshf <joshwalters049@gmail.com>
2024-03-05 20:31:25 +0000

    Corrected api code

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,53 +2,51 @@ import NextAuth from 'next-auth'
 import CredentialsProvider from 'next-auth/providers/credentials'
 import { connectToDatabase } from '../../../../../lib/db'
 import { verifyPassword } from '../../../../../lib/auth'
-import type { NextApiRequest, NextApiResponse } from 'next'
 
-async function POST(req: NextApiRequest, res: NextApiResponse) {
-  return NextAuth(req, res, {
-    session: {
-      strategy: 'jwt',
-    },
-    providers: [
-      CredentialsProvider({
-        name: 'Credentials',
-        credentials: {
-          email: { label: "Email", type: "email" },
-          password: { label: "Password", type: "password" },
-        },
-        async authorize(credentials, req) {
-          if (!credentials || !credentials.email || !credentials.password) {
-            return null;
-          }
+const handler = NextAuth({
+  session: {
+    strategy: 'jwt',
+  },
+  providers: [
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        if (!credentials || !credentials.email || !credentials.password) {
+          return null;
+        }
 
-          const client = await connectToDatabase();
-          const usersCollection = client.db().collection('users');
+        const client = await connectToDatabase();
+        const usersCollection = client.db().collection('users');
 
-          const user = await usersCollection.findOne({
-            email: credentials.email,
-          });
+        const user = await usersCollection.findOne({
+          email: credentials.email,
+        });
 
-          if (!user) {
-            client.close();
-            throw new Error('No user found!');
-          }
-
-          const isValid = await verifyPassword(credentials.password, user.password);
-
-          if (!isValid) {
-            client.close();
-            throw new Error('Could not log you in!');
-          }
-
+        if (!user) {
           client.close();
+          throw new Error('No user found!');
+        }
 
-          return {
-            id: user._id.toString(),
-            email: user.email,
-          };
-        },
-      }),
-    ],
-  });
-}
+        const isValid = await verifyPassword(credentials.password, user.password);
 
+        if (!isValid) {
+          client.close();
+          throw new Error('Could not log you in!');
+        }
+
+        client.close();
+
+        return {
+          id: user._id.toString(),
+          email: user.email,
+        };
+      },
+    }),
+  ],
+});
+
+export {handler as GET, handler as POST}
