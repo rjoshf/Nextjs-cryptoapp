65ebc4ce6ddb20cf6263a0264ff101dbfd4a8182
rjoshf <joshwalters049@gmail.com>
2024-03-06 18:06:18 +0000

    Added logout logic

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@
 
 import Link from 'next/link';
 import { useState, useEffect } from 'react';
-import { useSession } from 'next-auth/react';
+import { useSession, signOut } from 'next-auth/react';
 
 const Navbar: React.FC<{}> = ({ }) => {
     const { data: session, status } = useSession();
@@ -29,15 +29,18 @@ const Navbar: React.FC<{}> = ({ }) => {
         };
     }, []);
 
+    const logoutHandler = () => {
+        signOut();
+    }
+
     return (
         <>
             {isFixed && <div className="h-12 py-10 h-12" />}
             <nav className={`navbar flex flex-row items-center justify-between px-20 py-10 h-12 ${isFixed ? 'fixed z-10 top-0 w-full' : ''}`}>
                 <Link href="/" className="navlink text-white font-bold text-2xl">NextCrypto</Link>
                 <div>
-                    {!session && <><Link href="/login" className="navlink text-white font-bold text-2xl mr-10">Log In</Link>
-                        <Link href="/" className="navlink text-white font-bold text-2xl">Sign Up</Link></>}
-                    {session && <Link href="/">Logout</Link>}
+                    {!session && <Link href="/login" className="navlink text-white font-bold text-2xl mr-10">Log In</Link>}
+                    {session && <button onClick={logoutHandler}>Logout</button>}
                 </div>
             </nav>
         </>
