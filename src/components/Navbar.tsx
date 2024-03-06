5e0416e47430ddbd2a678e4052f0b210a8c0a220
rjoshf <joshwalters049@gmail.com>
2024-03-06 16:04:49 +0000

    Added logic for active session management on frontend

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,8 +2,12 @@
 
 import Link from 'next/link';
 import { useState, useEffect } from 'react';
+import { useSession } from 'next-auth/react';
 
 const Navbar: React.FC<{}> = ({ }) => {
+    const { data: session, status } = useSession();
+    const loading = status === 'loading';
+
     const [isFixed, setIsFixed] = useState(false);
 
     useEffect(() => {
@@ -17,6 +21,7 @@ const Navbar: React.FC<{}> = ({ }) => {
 
         window.addEventListener("scroll", handleScroll);
 
+        // Initial call for handleScroll to set the initial state based on the current scroll position
         handleScroll();
 
         return () => {
@@ -30,8 +35,9 @@ const Navbar: React.FC<{}> = ({ }) => {
             <nav className={`navbar flex flex-row items-center justify-between px-20 py-10 h-12 ${isFixed ? 'fixed z-10 top-0 w-full' : ''}`}>
                 <Link href="/" className="navlink text-white font-bold text-2xl">NextCrypto</Link>
                 <div>
-                    <Link href="/login" className="navlink text-white font-bold text-2xl mr-10">Log In</Link>
-                    <Link href="/" className="navlink text-white font-bold text-2xl">Sign Up</Link>
+                    {!session && <><Link href="/login" className="navlink text-white font-bold text-2xl mr-10">Log In</Link>
+                        <Link href="/" className="navlink text-white font-bold text-2xl">Sign Up</Link></>}
+                    {session && <Link href="/">Logout</Link>}
                 </div>
             </nav>
         </>
