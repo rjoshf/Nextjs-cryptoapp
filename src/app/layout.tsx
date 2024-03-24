92c301dbb205f2634ffd65b5529febe4c5a609ef
rjoshf <joshwalters049@gmail.com>
2024-03-24 19:42:30 +0000

    Created a portal for the modal

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <div id="portal-root"></div>
         <NextAuthProvider>
           <Navbar />
           {children}
--- src/components/UI/Modal.tsx
+++ src/components/UI/Modal.tsx
@@ -1,4 +1,5 @@
 import React, { useRef, MouseEvent } from 'react';
+import ReactDOM from 'react-dom';
 
 import { motion } from 'framer-motion';
 
@@ -10,12 +11,15 @@ const Modal: React.FC<{ open: boolean; children: React.ReactNode; onClose: () =>
 
     if (!open) return null;
 
-    return (
-        <div className="fixed top-0 left-0 z-10 w-full h-screen bg-black bg-opacity-60" onClick={onClose}>
-            <motion.div variants={{ hidden: { opacity: 0 }, visible: { opacity: 1 } }} initial="hidden" animate="visible" exit="hidden" className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 font-bold z-20 bg-purple-900 rounded-lg shadow-md animate-slide-down-fade-in duration-300 ease-out p-5 max-w-90 max-h-90vh overflow-auto" onClick={handleDialogClick} ref={dialogRef}>
-                {children}
-            </motion.div>
-        </div>
+    return ReactDOM.createPortal(
+        (
+            <div className="fixed top-0 left-0 z-10 w-full h-screen bg-black bg-opacity-60" onClick={onClose}>
+                <motion.div variants={{ hidden: { opacity: 0 }, visible: { opacity: 1 } }} initial="hidden" animate="visible" exit="hidden" className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 font-bold z-20 bg-purple-900 rounded-lg shadow-md animate-slide-down-fade-in duration-300 ease-out p-5 max-w-90 max-h-90vh overflow-auto" onClick={handleDialogClick} ref={dialogRef}>
+                    {children}
+                </motion.div>
+            </div>
+        ),
+        document.getElementById('portal-root')!
     );
 };
 
