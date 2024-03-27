a63e942f5c10bfac87ab7c080b1d309bf98634d2
rjoshf <joshwalters049@gmail.com>
2024-03-27 19:01:09 +0000

    Cleaned up some code

--- src/app/api/createuser/route.ts
+++ src/app/api/createuser/route.ts
@@ -1,19 +1,16 @@
-import { NextRequest, NextResponse } from "next/server";
 import { connectToDatabase } from "../../../../lib/db";
 import { hashPassword } from "../../../../lib/auth";
 
-export async function POST(req: NextRequest, res: NextResponse) {
+export async function POST(req: Request) {
     try {
         const data = await req.json();
 
         const { email, password } = data;
 
         if (!email || !email.includes('@') || !password || password.trim().length < 7) {
-            return NextResponse.json({
-                message: 'Invalid input - password should also be at least 7 characters long.',
-            }, {
+            return new Response(JSON.stringify({message: 'Invalid input - password should be at least 7 characters long'}), {
                 status: 500,
-            });
+            })
         }
 
         const client = await connectToDatabase();
@@ -24,11 +21,9 @@ export async function POST(req: NextRequest, res: NextResponse) {
 
         if (existingUser) {
             client.close();
-            return NextResponse.json({
-                message: 'User exists already!',
-            }, {
+            return new Response(JSON.stringify({message: 'User exists already!'}), {
                 status: 422,
-            });
+            })
         }
 
         //encrypt the password incase of a database leak
@@ -43,17 +38,14 @@ export async function POST(req: NextRequest, res: NextResponse) {
 
         client.close();
 
-        return NextResponse.json({
-            message: 'Created user!',
-        }, {
+        return new Response(JSON.stringify({message: 'Created User!'}), {
             status: 201,
-        });
+        })
+
     } catch (error) {
         console.error(error);
-        return NextResponse.json({
-            message: 'Internal server error.',
-        }, {
+        return new Response(JSON.stringify({message: 'Internal server error'}), {
             status: 500,
-        });
+        })
     }
 }
